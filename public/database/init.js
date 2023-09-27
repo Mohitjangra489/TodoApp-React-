cd836d15b53a7e19304faaa4561471d44b744a12
Mohit Jangra <119482895+Mohitjangra489@users.noreply.github.com>
2023-09-27 15:41:31 +0530

    Update init.js
    connected mongo to atlas

--- public/database/init.js
+++ public/database/init.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose");
 module.exports=async function init()
 {
     
-        await mongoose.connect('mongodb://127.0.0.1:27017/todoreact');
+        await mongoose.connect('mongodb+srv://mohit489:mohit489@cluster0.8ol98zz.mongodb.net/TodoReact');
         console.log(" MongoDB connected successfully");
 
    
